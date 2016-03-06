ee6dcc883e42782056a26c12de81267170d26f7a
Adam Cardenas <acardenas@twitter.com>
2016-03-06 11:12:27 -0700

    added users view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  def show
+    @user_activities = @user.user_activities
+  end
+
+  private
+    def set_user
+      @user = User.find_by_username(params[:username])
+    end
+end
